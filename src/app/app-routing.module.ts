32416010c26f05e66379f3e1f3cdc68f1c9b548b
dsjramesh7 <rameshdussa07@gmail.com>
2025-07-18 19:31:39 +0530

    practice: modules, component, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule) }, { path: 'contact', loadChildren: () => import('./features/contact/contact.module').then(m => m.ContactModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
