65d5be56e2754e72736b0ae3c1361ecaf0152e63
dsjramesh7 <rameshdussa07@gmail.com>
2025-08-02 21:20:39 +0530

    changes: localstorage add,delete and update

--- src/app/components/todo-component/todo-component.component.ts
+++ src/app/components/todo-component/todo-component.component.ts
@@ -9,14 +9,39 @@ export class TodoComponentComponent {
   taskTitle:string = "";
   todoList:string[] = [];
 
+  constructor(){
+    this.loadFromLocalStorage();
+  }
+
   handleAddTask():void{
     this.todoList.push(this.taskTitle);
     this.taskTitle = ""
+    this.saveToLocalStorage();
     // console.log("Todos: ", this.todoList);
   }
 
   handleDeleteTask(i:number):void{
     // this.todoList.splice(i,1)
     this.todoList = this.todoList.filter((_,taskIndex)=> taskIndex !== i)
+    this.saveToLocalStorage(); 
+  }
+
+  handleEditTask(i:number):void{
+    const newEditedTask = prompt("Enter the task change: ")
+    if(newEditedTask && newEditedTask.trim()){
+      this.todoList = this.todoList.map((item,itemIndex)=> itemIndex === i ? newEditedTask : item )
+    }
+    this.saveToLocalStorage(); 
+  }
+
+  saveToLocalStorage(){
+    localStorage.setItem('Todos',JSON.stringify(this.todoList))
+  }
+
+  loadFromLocalStorage(){
+    const savedTodos = localStorage.getItem('Todos')
+    if(savedTodos){
+      this.todoList = JSON.parse(savedTodos)
+    }
   }
 }
