d5a493b56de187776a2568b7e31f2aefda08a9b2
dsjramesh7 <rameshdussa07@gmail.com>
2025-08-03 00:49:15 +0530

    practice

--- src/app/components/todo-component/todo-component.component.ts
+++ src/app/components/todo-component/todo-component.component.ts
@@ -3,45 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-component',
   templateUrl: './todo-component.component.html',
-  styleUrl: './todo-component.component.scss'
+  styleUrl: './todo-component.component.scss',
 })
 export class TodoComponentComponent {
-  taskTitle:string = "";
-  todoList:string[] = [];
+  taskTitle: string = '';
+  todoList: string[] = [];
 
-  constructor(){
+  constructor() {
     this.loadFromLocalStorage();
   }
 
-  handleAddTask():void{
-    this.todoList.push(this.taskTitle);
-    this.taskTitle = ""
+  handleAddTask(): void {
+    if (this.taskTitle && this.taskTitle.trim()) {
+      this.todoList.push(this.taskTitle);
+    }
+    this.taskTitle = '';
     this.saveToLocalStorage();
     // console.log("Todos: ", this.todoList);
   }
 
-  handleDeleteTask(i:number):void{
+  handleDeleteTask(i: number): void {
     // this.todoList.splice(i,1)
-    this.todoList = this.todoList.filter((_,taskIndex)=> taskIndex !== i)
-    this.saveToLocalStorage(); 
+    this.todoList = this.todoList.filter((_, taskIndex) => taskIndex !== i);
+    this.saveToLocalStorage();
   }
 
-  handleEditTask(i:number):void{
-    const newEditedTask = prompt("Enter the task change: ")
-    if(newEditedTask && newEditedTask.trim()){
-      this.todoList = this.todoList.map((item,itemIndex)=> itemIndex === i ? newEditedTask : item )
+  handleEditTask(i: number): void {
+    const newEditedTask = prompt('Enter the task change: ');
+    if (newEditedTask && newEditedTask.trim()) {
+      this.todoList = this.todoList.map((item, itemIndex) =>
+        itemIndex === i ? newEditedTask : item
+      );
     }
-    this.saveToLocalStorage(); 
+    this.saveToLocalStorage();
   }
 
-  saveToLocalStorage(){
-    localStorage.setItem('Todos',JSON.stringify(this.todoList))
+  saveToLocalStorage() {
+    localStorage.setItem('Todos', JSON.stringify(this.todoList));
   }
 
-  loadFromLocalStorage(){
-    const savedTodos = localStorage.getItem('Todos')
-    if(savedTodos){
-      this.todoList = JSON.parse(savedTodos)
+  loadFromLocalStorage() {
+    const savedTodos = localStorage.getItem('Todos');
+    if (savedTodos) {
+      this.todoList = JSON.parse(savedTodos);
     }
   }
 }
